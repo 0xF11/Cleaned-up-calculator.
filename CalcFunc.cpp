851d08f58d144910df0763ae781a2834e95016fe
0xF11 <140972657+0xF11@users.noreply.github.com>
2025-05-29 18:42:01 -0700

    Create CalcFunc.cpp

---
+++ CalcFunc.cpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+
+void text(std::string messager)
+{
+	std::cout << messager;
+}
+
+void textel(std::string message)
+{
+	std::cout << message << std::endl;
+}
+void multilog(long long a, long long b)
+{
+	std::cout << a * b << std::endl;
+}
+void divlog(long long a, long long b)
+{
+	std::cout << a / b << std::endl;
+}
+void addlog(long long a, long long b)
+{
+	std::cout << a + b << std::endl;
+}
+void sublog(long long a, long long b)
+{
+	std::cout << a - b << std::endl;
+}
